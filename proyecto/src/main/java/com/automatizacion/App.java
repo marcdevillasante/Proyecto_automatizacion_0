4a587f7d618a9286b34b5468e472173db333b55d
Marc <marc.de.villasante@gmail.com>
2026-01-26 21:15:59 +0100

    creacio del proyecto

---
+++ proyecto/src/main/java/com/automatizacion/App.java
@@ -0,0 +1,13 @@
+package com.automatizacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
